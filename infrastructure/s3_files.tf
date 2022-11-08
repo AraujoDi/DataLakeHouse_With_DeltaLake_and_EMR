a58a7fbdde45bc743531ac6c082f21db842c1a14
AraujoDi <diaraujo_@hotmail.com>
2022-11-07 21:11:32 -0300

    Atualizando s3_files

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,17 +1,8 @@
 # Subindo arquivo para o S3
-resource "aws_s3_bucket_object" "delta_insert" {
+resource "aws_s3_bucket_object" "job_spark" {
     bucket = aws_s3_bucket.dl.id
-    key    = "emr-code/pyspark/01_delta_spark_insert.py"
+    key    = "emr-code/pyspark/job_spark_from_tf.py"
     acl    = "private"
-    source = "../etl/01_delta_spark_insert.py"
-    etag   = filemd5("../etl/01_delta_spark_insert.py")
-}
-
-# Subindo arquivo para o S3
-resource "aws_s3_bucket_object" "delta_upsert" {
-    bucket = aws_s3_bucket.dl.id
-    key    = "emr-code/pyspark/02_delta_spark_upsert.py"
-    acl    = "private"
-    source = "../etl/02_delta_spark_upsert.py"
-    etag   = filemd5("../etl/02_delta_spark_upsert.py")
+    source = "../job_spark.py"
+    etag   = filemd5("../job_spark.py")
 }
