87c8563cb731a53df743bab75d18a8384d7640ff
AraujoDi <diaraujo_@hotmail.com>
2022-12-07 22:14:26 -0300

    Atualização de arquivo lambda.

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -5,8 +5,10 @@ resource "aws_lambda_function" "executa_emr" {
     handler       = "lambda_function.handler"
     memory_size   = 128
     timeout       = 30
-}
+
 
 source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
 runtime = "python3.8"
+
+}
